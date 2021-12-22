30daa3b1ed68b9084a531a4780c040289480d1c5
LCODVME <2685981436@qq.com>
2021-12-22 23:21:59 +0800

    Change zm led and key default num set

--- ZMOS/ZmDrivers/common/zm_driverConfig.h
+++ ZMOS/ZmDrivers/common/zm_driverConfig.h
@@ -39,7 +39,7 @@ extern "C"
  *       The larger the value, the more space it takes up.
  */
 #ifndef ZM_LED_MAX_NUM
-#define ZM_LED_MAX_NUM          2
+#define ZM_LED_MAX_NUM          1
 #endif
     
 #if ZM_LED_MAX_NUM > 0
@@ -65,7 +65,7 @@ extern "C"
  *       The larger the value, the more space it takes up.
  */
 #ifndef ZM_KEY_MAX_NUM
-#define ZM_KEY_MAX_NUM          4
+#define ZM_KEY_MAX_NUM          1
 #endif
     
 #if ZM_KEY_MAX_NUM > 0
