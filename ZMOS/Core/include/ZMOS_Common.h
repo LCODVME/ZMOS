4f97b1b8f2e36ec4d547118f5ea79929680b50cb
LCODVME <2685981436@qq.com>
2022-07-10 20:01:40 +0800

    Add some functional macros

--- ZMOS/Core/include/ZMOS_Common.h
+++ ZMOS/Core/include/ZMOS_Common.h
@@ -36,7 +36,7 @@ extern "C"
  * ZMOS null define.
  */
 #ifndef NULL
-#define NULL   0
+#define NULL   (void *)0
 #endif
     
 /**
@@ -70,6 +70,25 @@ extern "C"
 #define BC(n)   ZMOS_BC(n)
 #endif
 
+    
+/**
+ * The upper 8 bits of a 32 bit value.
+ */
+#define ZMOS_MSB_32(a)                  (((a) & 0xFF000000) >> 24)
+/**
+ * The lower 8 bits (of a 32 bit value).
+ */
+#define ZMOS_LSB_32(a)                  ((a) & 0x000000FF)
+
+/**
+ * The upper 8 bits of a 16 bit value.
+ */
+#define ZMOS_MSB_16(a)                  (((a) & 0xFF00) >> 8)
+/**
+ * The lower 8 bits (of a 16 bit value).
+ */
+#define ZMOS_LSB_16(a)                  ((a) & 0x00FF)
+    
 /**
  * ZMOS Value plus one, and hold the 8-bit maximum.
  */
@@ -96,10 +115,80 @@ extern "C"
  */
 #define ZMOS_GET_MIN(a, b)            ((a) < (b) ? (a) : (b))
     
+    
 /**
- * ZMOS general return codes.
+ * Concatenates two parameters.
  */
-//#define ZMOS_
+#define CONCAT_2(p1, p2)      CONCAT_2_(p1, p2)
+/** Auxiliary macro used by @ref CONCAT_2 */
+#define CONCAT_2_(p1, p2)     p1##p2
+/**
+ * Concatenates three parameters.
+ */
+#define CONCAT_3(p1, p2, p3)  CONCAT_3_(p1, p2, p3)
+/** Auxiliary macro used by @ref CONCAT_3 */
+#define CONCAT_3_(p1, p2, p3) p1##p2##p3
+/**
+ * Converts a macro argument into a character constant.
+ */   
+#define STRINGIFY(val)  STRINGIFY_(val)
+/** Auxiliary macro used by @ref STRINGIFY */
+#define STRINGIFY_(val) #val    
+    
+/**
+ * Counts number of elements inside the array.
+ */
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+    
+#ifndef __LINT__
+
+#if defined(__CC_ARM)
+#define STATIC_ASSERT_SIMPLE(EXPR)      extern char (*_do_assert(void)) [sizeof(char[1 - 2*!(EXPR)])]
+#define STATIC_ASSERT_MSG(EXPR, MSG)    extern char (*_do_assert(void)) [sizeof(char[1 - 2*!(EXPR)])]
+
+#elif defined(__GNUC__)
+#ifdef __cplusplus
+#define STATIC_ASSERT_SIMPLE(EXPR)      extern char (*_do_assert(void)) [sizeof(char[1 - 2*!(EXPR)])]
+#define STATIC_ASSERT_MSG(EXPR, MSG)    extern char (*_do_assert(void)) [sizeof(char[1 - 2*!(EXPR)])]
+#else
+#define STATIC_ASSERT_SIMPLE(EXPR)      _Static_assert(EXPR, "unspecified message")
+#define STATIC_ASSERT_MSG(EXPR, MSG)    _Static_assert(EXPR, MSG)
+#endif
+
+#elif defined(__ICCARM__)
+#define STATIC_ASSERT_SIMPLE(EXPR)      static_assert(EXPR, "unspecified message")
+#define STATIC_ASSERT_MSG(EXPR, MSG)    static_assert(EXPR, MSG)
+#else
+#error "Not compatible??"
+#endif
+
+#else // __LINT__
+
+#define STATIC_ASSERT_SIMPLE(EXPR)      extern char (*_ignore(void))
+#define STATIC_ASSERT_MSG(EXPR, MSG)    extern char (*_ignore(void))
+
+#endif
+    
+
+/**
+ * @brief   Static (i.e. compile time) assert macro.
+ *
+ * @note The output of STATIC_ASSERT can be different across compilers.
+ *
+ * Usage:
+ * STATIC_ASSERT(expression);
+ * STATIC_ASSERT(expression, message);
+ *
+ * @hideinitializer
+ */
+//lint -save -esym(???, STATIC_ASSERT)
+#define STATIC_ASSERT(...)                                                                          \
+    _SELECT_ASSERT_FUNC(x, ##__VA_ARGS__,                                                           \
+                        STATIC_ASSERT_MSG(__VA_ARGS__),                                             \
+                        STATIC_ASSERT_SIMPLE(__VA_ARGS__))
+
+
+#define _SELECT_ASSERT_FUNC(x, EXPR, MSG, ASSERT_MACRO, ...) ASSERT_MACRO
 /*************************************************************************************************************************
  *                                                      CONSTANTS                                                        *
  *************************************************************************************************************************/
